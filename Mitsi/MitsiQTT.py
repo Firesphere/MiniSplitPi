9a000b2acc64739905cd08814bf1ef9376fec1aa
Simon Erkelens <github@casa-laguna.net>
2025-03-07 21:59:15 +1300

    Just some ordering of things

--- Mitsi/MitsiQTT.py
+++ Mitsi/MitsiQTT.py
@@ -20,24 +20,26 @@ class MitsiQTT:
     password = ''
     connected_state = 0
     client = mqtt
-    state = State
+    state = None
     logger = logging
 
-    def __init__(self, logger):
+    def __init__(self, logger, **kwargs):
         signal.signal(signal.SIGINT, self.cleanup)
         signal.signal(signal.SIGTERM, self.cleanup)
         self._load_env()
         self.running = True
         self.logger = logger
         self._setup_heatpump()
-        self._setup_state()
+        self._setup_state(**kwargs)
         self._setup_client()
 
     def _setup_heatpump(self):
         self.hp = HeatPump('/dev/ttyAMA0')
 
-    def _setup_state(self):
-        self.state = State()
+    def _setup_state(self, **kwargs):
+        if self.state is None:
+            self.state = State()
+        self.state.update(kwargs)
 
     def _setup_client(self):
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, protocol=mqtt.MQTTv31)
@@ -50,7 +52,7 @@ class MitsiQTT:
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.connect(self.host, port=self.port)
-        self.talk("connected", 1, qos=1, retain=True)
+        self.on_publish("connected", 1, qos=1, retain=True)
         self.connected_state = 1
         self.client.subscribe("%s/command/#" % self.topic)
 
@@ -66,39 +68,13 @@ class MitsiQTT:
         if rc.is_failure:
             raise Exception("Connectivity to MQTT broker failed")
         self.logger.info("Connected to MQTT broker: %s", self.host)
-        self.talk("connected", 1, qos=1, retain=True)
+        self.on_publish("connected", 1, qos=1, retain=True)
         self.connected_state = 1
         self.client.subscribe("%s/command/#" % self.topic)
         self.hp.connect()
         self.hp.dirty = True
 
-    def run(self):
-        while self.running:
-            self.hp.loop()
-            if self.hp.valid and self.hp.dirty:
-                if self.connected_state != 2:
-                    self.talk("connected", 2, qos=1, retain=True)
-                    self.connected_state = 2
-                self.state.update(self.hp.to_dict())
-                self.talk("state", json.dumps(self.state.reverse_state()), retain=True)
-                self.hp.dirty = False
-            if self.client.loop() != 0:
-                self.logger.warning("Disonnected from MQTT broker: %s", self.host)
-                self.connected_state = 0
-                try:
-                    self.client.connect(self.host)
-                    self.connected_state = 0
-                except Exception:
-                    self.logger.error("Failed reconnecting to broker!")
-                    time.sleep(1)
-
-    def cleanup(self, signum, frame):
-        self.client.disconnect()
-        self.running = False
-        self.connected_state = False
-        sys.exit(signum)
-
-    def talk(self, topic, payload, **kwargs):
+    def on_publish(self, topic, payload, **kwargs):
         topic = "%s/%s" % (self.topic, topic)
         self.client.publish(topic, payload, **kwargs)
 
@@ -116,3 +92,29 @@ class MitsiQTT:
                 self.logger.warning("Invalid JSON: %s" % msg.payload)
             except Exception:
                 self.logger.error("Failed to set state %s" % json.dumps(state))
+
+    def run(self):
+        while self.running:
+            self.hp.loop()
+            if self.hp.valid and self.hp.dirty:
+                if self.connected_state != 2:
+                    self.on_publish("connected", 2, qos=1, retain=True)
+                    self.connected_state = 2
+                self.state.update(self.hp.to_dict())
+                self.on_publish("state", json.dumps(self.state.reverse_state()), retain=True)
+                self.hp.dirty = False
+            if self.client.loop() != 0:
+                self.logger.warning("Disonnected from MQTT broker: %s", self.host)
+                self.connected_state = 0
+                try:
+                    self.client.connect(self.host)
+                    self.connected_state = 0
+                except Exception:
+                    self.logger.error("Failed reconnecting to broker!")
+                    time.sleep(1)
+
+    def cleanup(self, signum, frame):
+        self.client.disconnect()
+        self.running = False
+        self.connected_state = False
+        sys.exit(signum)
